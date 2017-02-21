a8580c80c692b4efa466fbe70ef1580bee729ddb
Yusuke Koda <yusuke.koda@ansys.com>
2017-02-21 16:20:54 -0500

    Updating phong lighting compute shader.

--- LBM_GL_CPP/ComputeShader.glsl
+++ LBM_GL_CPP/ComputeShader.glsl
@@ -11,9 +11,13 @@ uniform int maxXDim;
 uniform int maxyDim;
 uniform vec3 cameraPosition;
 
+subroutine void VboUpdate(uvec3 workUnit);
+
+
 vec4 unpackColor(float f)
 {
     uint f2 = floatBitsToUint(f);
+
 
     uint r = (f2 & uint(0x000000FF));
     uint g = (f2 & uint(0x0000FF00)) >> 8;
@@ -25,10 +29,10 @@ vec4 unpackColor(float f)
     float bf = float(b);
     float af = float(a);
     vec4 color;
-    color.x = rf/256.0;
-    color.y = gf/256.0;
-    color.z = bf/256.0;
-    color.w = af/256.0;
+    color.x = rf/255.0;
+    color.y = gf/255.0;
+    color.z = bf/255.0;
+    color.w = af/255.0;
 
     return color;
 }
@@ -46,24 +50,23 @@ float packColor(vec4 color)
 
 void Normalize(inout vec3 n)
 {
-    float mag = sqrt(n.x*n.x + n.y*n.y + n.z*n.z);
-    n /= mag;
+	n = normalize(n);
 }
 
 float DotProduct(vec3 v1, vec3 v2)
 {
-    return v1.x*v2.x + v1.y*v2.y + v1.z*v2.z;
+    return dot(v1,v2);
 }
 
-vec3 PhongLighting()
+subroutine(VboUpdate) void PhongLighting(uvec3 workUnit)
 {
-    uint x = gl_GlobalInvocationID.x;
-    uint y = gl_GlobalInvocationID.y;
+	uint x = workUnit.x;
+	uint y = workUnit.y;
     uint j = x + y * maxXDim;
 
     float slope_x = 0.f;
     float slope_y = 0.f;
-    float cellSize = 2.f / 128;
+    float cellSize = 2.f / xDimVisible;
     vec3 n;
 
     slope_x = (positions[(x + 1) + y*maxXDim].z - positions[(x - 1) + y*maxXDim].z) /
@@ -104,37 +107,24 @@ vec3 PhongLighting()
     vec3 diffuse2  = 0.1f*cosTheta2*diffuseLightColor2;
     vec3 specular1 = cosAlpha*specularLightColor1;
     
-    vec3 lightFactor;
-    lightFactor.x = min(1.f, (diffuse1.x + diffuse2.x + specular1.x + lightAmbient));
-    lightFactor.y = min(1.f, (diffuse1.y + diffuse2.y + specular1.y + lightAmbient));
-    lightFactor.z = min(1.f, (diffuse1.z + diffuse2.z + specular1.z + lightAmbient));
+    vec4 lightFactor;
+    lightFactor.xyz = min(vec3(1.f,1.f,1.f), (diffuse1.xyz + diffuse2.xyz + specular1.xyz + lightAmbient));
+	lightFactor.w = 1.f;
 
-    return lightFactor;
+    vec4 unpackedColor = unpackColor(positions[j].w);
+    vec4 finalColor;
+    finalColor.xyzw = unpackedColor.xyzw*lightFactor.xyzw;
+
+    positions[j].w = packColor(finalColor);
 }
 
 
 void main()
 {
-    uint xi = gl_GlobalInvocationID.x;
-    uint yi = gl_GlobalInvocationID.y;
-    uint j = xi + yi*maxXDim;
 
-    vec4 unpackedColor = unpackColor(positions[j].w);
+	//VboUpdate(gl_GlobalInvocationID);
+    PhongLighting(gl_GlobalInvocationID);
 
-    vec3 colorFactor = PhongLighting();
 
-    vec4 finalColor;
-
-    finalColor.x = unpackedColor.x*colorFactor.x;
-    finalColor.y = unpackedColor.y*colorFactor.y;
-    finalColor.z = unpackedColor.z*colorFactor.z;
-    finalColor.w = unpackedColor.w;
-
-    float x = positions[j].x;
-    float y = positions[j].y;
-    float z = positions[j].z;
-    float w = packColor(finalColor);
-
-    positions[j] = vec4(x,y,z,w);
 
 }
