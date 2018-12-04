2a420423bed7564c3769301ad868c7c8360ea569
ykoda <ykoda630@gmail.com>
2018-12-03 22:56:08 -0500

    Contour range adjustments.

--- Shizuku/Window.cpp
+++ Shizuku/Window.cpp
@@ -403,17 +403,19 @@ void Window::DrawUI()
             const char* format = "%.3f";
             switch (m_contourMode){
             case VelocityMagnitude:
+                maxRange = MinMax<float>(0.f, 4.f*m_velocity);
+                break;
             case VelocityU:
-                maxRange = MinMax<float>(0.f, 2.5f*m_velocity);
+                maxRange = MinMax<float>(-1.5f*m_velocity, 4.f*m_velocity);
                 break;
             case VelocityV:
-                maxRange = MinMax<float>(-m_velocity, m_velocity);
+                maxRange = MinMax<float>(-2.f*m_velocity, 2.f*m_velocity);
                 break;
             case Pressure:
-                maxRange = MinMax<float>(0.99f, 1.01f);
+                maxRange = MinMax<float>(0.98f, 1.02f);
                 break;
             case StrainRate:
-                maxRange = MinMax<float>(0.f, m_velocity*m_velocity);
+                maxRange = MinMax<float>(0.f, 2.f*m_velocity*m_velocity);
                 format = "%.5f";
                 break;
             }
